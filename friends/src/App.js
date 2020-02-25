b41ffd66a6baa8d13a3b7a2751c7806b0ec8f284
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-02-25 17:14:12 -0500

    Added login page

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 
-import {Grommet, Box, Anchor} from 'grommet';
+import { Grommet, Box, Anchor } from 'grommet';
 import theme from './theme';
-import {Favorite} from 'grommet-icons';
+import { Favorite } from 'grommet-icons';
 
+import PrivateRoute from './components/PrivateRoute';
 import AppBar from './components/AppBar';
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
 
 const StyledLink = (props) => (
-  <Link to={props.to} style={{textDecoration: 'none'}}>
+  <Link to={props.to} style={{ textDecoration: 'none' }}>
     <Anchor margin='medium'> {props.children} </Anchor>
   </Link>
 )
@@ -18,18 +21,20 @@ const StyledLink = (props) => (
 function App() {
   return (
     <Router>
-   <Grommet theme={theme}>
-      <Box flex>
-        <AppBar icon={<Favorite/>}>
-          <StyledLink to='/friendslist'>Friendslist</StyledLink>
-          <StyledLink to='/login'>Login</StyledLink>
-        </AppBar>
-        <Switch>
-          
-        </Switch>
-      </Box>
-   </Grommet>
-   </Router>
+      <Grommet theme={theme}>
+        <Box flex>
+          <AppBar icon={<Favorite />}>
+            <StyledLink to='/friendslist'>Friendslist</StyledLink>
+            <StyledLink to='/login'>Login</StyledLink>
+          </AppBar>
+          <Switch>
+            <PrivateRoute path="/friendslist" component={FriendsList} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+          </Switch>
+        </Box>
+      </Grommet>
+    </Router>
 
   );
 }
---
+++ friends/src/components/Login.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
+
+import {Box, Form, FormField, Button, Heading} from 'grommet';
+import { axiosWithAuth } from '../utils/AxiosWithAuth';
+
+const Login = (props) => {
+    let history = useHistory();
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleChange = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const login = (e) => {
+        e.preventDefault()
+        axiosWithAuth()
+            .post('api/login', credentials)
+            .then(res => {
+                window.localStorage.setItem('token', res.data.payload)
+                history.push('/friendslist')
+            })
+            .catch(err => console.error(err))
+    }
+
+    return (
+        <Box flex direction='row' justify='center' pad={{vertical: '180px'}}>
+            <Form onSubmit={(e) => login(e)}>
+                <FormField name="username" label="Username" onChange={(e) => handleChange(e)} />
+                <FormField name="password" label="Password" onChange={(e) => handleChange(e)} type='password' />
+                <Button type="submit" primary label="Submit" />
+            </Form>
+        </Box>
+    )
+}
+
+export default Login;
